969b2315198576219eb26d7355a5eb9087555d7c
farokh ebrahimi nejad <133846533+farokh7@users.noreply.github.com>
2024-08-07 19:18:19 +0330

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\FromController;
 use App\Http\Controllers\FromInfoController;
 use App\Http\Controllers\HesabController;
+use App\Http\Controllers\CostController;
 
 use App\Http\Controllers\HoghoghController;
 use Illuminate\Support\Facades\Route;
@@ -32,7 +33,15 @@ Route::post('/from/store',[FromController::class,'store'])->Name('fromstore');
 
 Route::get('/salary',[HoghoghController::class,'index'])->name('salary');
 Route::post('/salary/store',[HoghoghController::class,'store'])->Name('salarystore');
+Route::get('/salary/delete/{id}',[HoghoghController::class,'delete'])->Name('salarydelet');
+Route::get('/salary/update/{id}',[HoghoghController::class,'update'])->Name('salaryupdate');
 
 
-Route::get('/cost',[HesabController::class,'costshow'])->name('cost');
-Route::post('/cost/store',[HesabController::class,'coststore'])->Name('coststore');
+
+
+Route::get('/cost',[CostController::class,'index'])->name('cost');
+Route::post('/cost/store',[CostController::class,'coststore'])->Name('coststore');
+Route::get('/cost/delete/{id}',[CostController::class,'delete'])->Name('costdelet');
+Route::get('/cost/update/{id}',[CostController::class,'update'])->Name('costupdate');
+
+
