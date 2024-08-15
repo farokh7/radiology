ebea635fd7bdc2f93287a7160fab786097b96134
farokh ebrahimi nejad <133846533+farokh7@users.noreply.github.com>
2024-08-15 18:49:26 +0330

    Add files via upload

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http;
+use App\Http\Middleware\User11;
+
+
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -63,5 +66,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        
+        'Userlogin'=> Userlogin::class,
+        'user'=>User11::class,
     ];
 }
