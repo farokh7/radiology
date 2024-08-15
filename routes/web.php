4cb1c6951b1cc05b90e184d9b45c2ef39496dd0e
farokh ebrahimi nejad <133846533+farokh7@users.noreply.github.com>
2024-08-15 18:48:22 +0330

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\HesabController;
 use App\Http\Controllers\CostController;
 use App\Http\Controllers\Logincontrroler;
 
+use App\Http\Middleware;
+use App\Http\Middleware\User11;
+use Illuminate\Support\Facades\Auth;
+
 use App\Http\Controllers\HoghoghController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,29 +28,34 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/start',[HesabController::class,'index'])->name('income');
-Route::post('/store',[HesabController::class,'store'])->name('store');
+Route::get('/start',[HesabController::class,'index'])->name('income')->middleware('user');
+Route::post('/store',[HesabController::class,'store'])->name('store')->middleware('user');
 
-Route::get('/from',[FromController::class,'index'])->name('from');
-Route::get('/from/info',[FromInfoController::class,'index'])->name('frominfo');
-Route::post('/from/store',[FromController::class,'store'])->Name('fromstore');
-
-
-Route::get('/salary',[HoghoghController::class,'index'])->name('salary');
-Route::post('/salary/store',[HoghoghController::class,'store'])->Name('salarystore');
-Route::get('/salary/delete/{id}',[HoghoghController::class,'delete'])->Name('salarydelet');
-Route::get('/salary/update/{id}',[HoghoghController::class,'update'])->Name('salaryupdate');
+Route::get('/from',[FromController::class,'index'])->name('from')->middleware('user');
+Route::get('/from/info',[FromInfoController::class,'index'])->name('frominfo')->middleware('user');
+Route::post('/from/store',[FromController::class,'store'])->Name('fromstore')->middleware('user');
 
 
+Route::get('/salary',[HoghoghController::class,'index'])->name('salary')->middleware('user');
+Route::post('/salary/store',[HoghoghController::class,'store'])->Name('salarystore')->middleware('user');
+Route::get('/salary/delete/{id}',[HoghoghController::class,'delete'])->Name('salarydelet')->middleware('user');
+Route::get('/salary/update/{id}',[HoghoghController::class,'update'])->Name('salaryupdate')->middleware('user');
 
 
-Route::get('/cost',[CostController::class,'index'])->name('cost');
-Route::post('/cost/store',[CostController::class,'coststore'])->Name('coststore');
-Route::get('/cost/delete/{id}',[CostController::class,'delete'])->Name('costdelet');
-Route::get('/cost/update/{id}',[CostController::class,'update'])->Name('costupdate');
+
+
+Route::get('/cost',[CostController::class,'index'])->name('cost')->middleware('user');
+Route::post('/cost/store',[CostController::class,'coststore'])->Name('coststore')->middleware('user');
+Route::get('/cost/delete/{id}',[CostController::class,'delete'])->Name('costdelet')->middleware('user');
+Route::get('/cost/update/{id}',[CostController::class,'update'])->Name('costupdate')->middleware('user');
 
 
 
 Route::get('/login',[Logincontrroler::class,'index'])->name('login');
- 
+Route::post('/regeditstor',[Logincontrroler::class,'store'])->name('userregedit')->middleware('user');
+Route::get('/regedit',[Logincontrroler::class,'regeditshow'])
+->name('regedit')->middleware('user');
+Route::get('/logout',[Logincontrroler::class,'logout'])->name('logout');
+
+Route::post('/checkuser',[Logincontrroler::class,'checkuser'])->name('checkuser');
 
