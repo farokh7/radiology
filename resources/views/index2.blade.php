5c3d3c6bcef522edbe0dd343ea7571d3f7c95e78
farokh ebrahimi nejad <133846533+farokh7@users.noreply.github.com>
2024-09-17 15:49:06 +0330

    index ardiology

---
+++ resources/views/index2.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>مرکز تصویربرداری و رادیولوژی فرخ</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+
+            </style>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+
+            }
+            #header{
+                width: 100%;
+                height: 300px;
+
+            }
+            #header span{
+                float: right;
+            }
+            #header img{
+                float: right;
+            }
+        </style>
+    </head>
+    <body >
+
+    <div id="header">
+<img src="">
+<span> 
+اولین مرکز تصویربرداری
+ فوری در ایران</span>
+
+    </div>
+
+    <div id="menu"></div>
+    <div id="about"></div>
+    <div id="newstop"></div>
+    <div id="news"></div>
+    <div id="cooperationtop"></div>
+    <div id="cooperation"></div>
+    <div id="footer"></div>
+    <div id="copyright"></div>
+    
+    </body>
+</html>
